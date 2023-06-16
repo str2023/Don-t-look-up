0346ff8d82cbf314432edbe28e095ebcf3de575b
[AI7/2반] 손태린 <elice.str@gmail.com>
2023-06-17 08:34:06 +0900

    :white_check_mark: Test ip https

--- front/src/lib/apis/api.js
+++ front/src/lib/apis/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const backendPortNumber = '5001';
 // const serverUrl = `http://${window.location.hostname}:${backendPortNumber}`;
-const serverUrl = `http://34.64.46.101:${backendPortNumber}`;
+const serverUrl = `https://34.64.46.101:${backendPortNumber}`;
 
 async function get(endpoint, params) {
   console.log('params', params);
